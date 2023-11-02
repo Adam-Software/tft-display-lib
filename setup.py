e214de8c3ca420d7e786b507b7484d0a86754129
Editscode <38367082+Editscode@users.noreply.github.com>
2023-11-02 09:28:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ with open("README.md", "r", encoding = "utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name='robot_eye_display',
+    name='robot-eye-display',
     version='0.1.1',
-    packages=['serial_motor_control'],
+    packages=['robot_eye_display'],
     url='https://github.com/Adam-Software/tft-display-lib',
     license='MIT',
     author='vertigra',
@@ -14,7 +14,7 @@ setup(
     description='Motion control used old Api for STM32 firmware',
     long_description_content_type="text/markdown",
     long_description=long_description,
-    install_requires=['Robot_Eye_Display'],
+    install_requires=['Adam-display-for-controller'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
