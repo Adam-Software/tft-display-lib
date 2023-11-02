b0f8c5cbea10ba54382111e50d6f738611251cbb
Editscode <38367082+Editscode@users.noreply.github.com>
2023-11-02 10:06:27 +0300

    Update Example

--- ExampleRobotEyeDisplay.py
+++ ExampleRobotEyeDisplay.py
@@ -3,9 +3,9 @@ from robot_eye_display import RobotEyeDisplay
 robot_eye_display = RobotEyeDisplay()
 
 gif_paths_R = [
-    '../pic/ROBOT ADAM EYES/Adam-Classic-eyes-радуется.gif'
+    'ExampleGIF/Смущен/LEFT/Adam-Black-eyes-смущен-LEFT.gif'
 ]
 gif_paths_L = [
-    '../pic/ROBOT ADAM EYES/Adam-Classic-eyes-радуется.gif'
+    'ExampleGIF/Смущен/RIGHT/Adam-Black-eyes-смущен-RIGHT.gif'
 ]
 robot_eye_display.run(gif_paths_R, gif_paths_L)
