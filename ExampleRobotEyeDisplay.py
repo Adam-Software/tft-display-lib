b2d6604491ad43f54fad99bfbb2bed11aa4a1e6c
Editscode <38367082+Editscode@users.noreply.github.com>
2023-11-02 09:42:17 +0300

    Update ExampleRobotEyeDisplay.py

--- ExampleRobotEyeDisplay.py
+++ ExampleRobotEyeDisplay.py
@@ -1,1 +1,11 @@
-ExampleMotorControl
+import RobotEyeDisplay
+
+robot_eye_display = RobotEyeDisplay()
+
+gif_paths_R = [
+    '../pic/ROBOT ADAM EYES/Adam-Classic-eyes-радуется.gif'
+]
+gif_paths_L = [
+    '../pic/ROBOT ADAM EYES/Adam-Classic-eyes-радуется.gif'
+]
+robot_eye_display.run(gif_paths_R, gif_paths_L)
