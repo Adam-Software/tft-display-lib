5c927c15cc8c5236bbc5d5af45d12633b9100a7f
Alexey Nesterov <vertigra.developer@gmail.com>
2023-11-04 19:29:13 +1000

    Fix setup.py Added .gitignore Change version

--- setup.py
+++ setup.py
@@ -5,16 +5,15 @@ with open("README.md", "r", encoding = "utf-8") as fh:
 
 setup(
     name='robot-eye-display',
-    version='0.1.1',
+    version='0.0.1',
     packages=['robot_eye_display'],
     url='https://github.com/Adam-Software/tft-display-lib',
-    license='Unknown',
+    license='MIT',
     author='vertigra',
     author_email='a@nesterof.com',
     description='Robot eye display use for TFT-display 1.28 inch',
     long_description_content_type="text/markdown",
     long_description=long_description,
-    install_requires=['Adam-display-for-controller'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved ::Unknown',
